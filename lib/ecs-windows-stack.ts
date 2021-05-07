36dddb18591a672787cee38abc8ba01186aced64
Michael Tuszynski <tuszym@amazon.com>
2021-05-07 12:55:47 -0700

    update to working version

--- lib/ecs-windows-stack.ts
+++ lib/ecs-windows-stack.ts
@@ -2,30 +2,33 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import { ApplicationLoadBalancedEc2Service } from '@aws-cdk/aws-ecs-patterns';
+import * as logs from "@aws-cdk/aws-logs";
 
 export class ECSWindowsStack extends cdk.Stack {
 
   constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
+
     super(scope, id, props);
 
     const cluster = new ecs.Cluster(this, 'Cluster', {
-      clusterName: "ecs-windows"
+      clusterName: "ecs-windows-demo"
     });
 
-    const asg = cluster.addCapacity('WindowsEcsNodeGroup', {
+    const asg = cluster.addCapacity('WinEcsNodeGroup', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.LARGE),
       machineImage: ecs.EcsOptimizedImage.windows(ecs.WindowsOptimizedVersion.SERVER_2019),
       minCapacity: 1,
-      desiredCapacity: 1,
-      maxCapacity: 6,
+      desiredCapacity: 3,
+      maxCapacity: 3,
       canContainersAccessInstanceRole: false
     });
 
-    const security_group = new ec2.SecurityGroup(this, "win-ecs-security-group", {
+    const security_group = new ec2.SecurityGroup(this, "winEcs-security-group", {
       vpc: cluster.vpc
     });
 
-    security_group.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(5002));
+    security_group.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(5000));
+    security_group.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80));
 
     asg.addSecurityGroup(security_group);
 
@@ -36,30 +39,35 @@ export class ECSWindowsStack extends cdk.Stack {
       networkMode: ecs.NetworkMode.NAT
     });
 
-    const container = task.addContainer("WebAPI", {
-      //image: ecs.ContainerImage.fromRegistry("mptaws/dn-api-server:latest"),
-      image: ecs.ContainerImage.fromRegistry("microsoft/iis"),
+    const logGroup = new logs.LogGroup(this, "TodoAPILogging", {
+      retention: logs.RetentionDays.ONE_WEEK,
+    })
+
+    const logGroupName = logGroup.logGroupName;
+
+    const container = task.addContainer("TodoAPI", {
+      image: ecs.ContainerImage.fromRegistry("mptaws/dnapiserver"),
       memoryLimitMiB: 2048,
       cpu: 1024,
-      entryPoint: ["powershell", "-Command"],
-      command: ["New-Item -Path C:\\inetpub\\wwwroot\\index.html -ItemType file -Value '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p>' -Force ; C:\\ServiceMonitor.exe w3svc"],
-      essential: true
+      entryPoint: ["dotnet", "api.dll"],
+      essential: true,
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: "TodoAPI",
+        logGroup: logGroup,
+      })
     });
 
     container.addPortMappings({
-      containerPort: 80,
-      hostPort: 8080,
-      protocol: ecs.Protocol.TCP
+      containerPort: 5000
     });
 
-
-    const ecsEc2Service = new ApplicationLoadBalancedEc2Service(this, 'demoapp-service', {
+    const ecsEc2Service = new ApplicationLoadBalancedEc2Service(this, 'demoapp-service-demo', {
       cluster,
       cpu: 1024,
-      desiredCount: 1,
+      desiredCount: 3,
       minHealthyPercent: 50,
       maxHealthyPercent: 300,
-      serviceName: 'winapp-service',
+      serviceName: 'winapp-service-demo',
       taskDefinition: task,
       publicLoadBalancer: true
     });
